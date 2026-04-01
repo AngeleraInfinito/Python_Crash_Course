0a00f99aa4e1877ad5cc2c8e95d03c433d0d3324
AngeleraInfinito <Kritikahdy@gmail.com>
2026-04-01 19:24:38 +0530

    Create whitespace.py

---
+++ whitespace.py
@@ -0,0 +1,11 @@
+# to add 'tab'
+print("\tpython")
+
+# to add new line
+print("Languages:\n\tPython\n\tC++\n\tJavaScript")
+
+# to strip whitespace
+word = "  python  "
+print(word.rstrip())
+print(word.lstrip())
+print(word.strip())
