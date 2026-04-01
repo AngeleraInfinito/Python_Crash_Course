b712035fd7418d4d60194b0aac0e71d6bf613c68
AngeleraInfinito <Kritikahdy@gmail.com>
2026-04-01 19:20:10 +0530

    Create name_2.py

---
+++ name_2.py
@@ -0,0 +1,12 @@
+# CHANGING CASES USING METHODS
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+# COMBINING STRINGS = CONCATENATION
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+
