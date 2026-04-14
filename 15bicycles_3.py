c7bab010f9b3de64fb56c14cacb5285133e411d3
AngeleraInfinito <Kritikahdy@gmail.com>
2026-04-14 12:25:08 +0530

    Create 15bicycles_3.py

---
+++ 15bicycles_3.py
@@ -0,0 +1,14 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+# accessing elements in a list
+print(bicycles[0])
+print(bicycles[0].title())
+
+# last item
+print(bicycles[-1])
+
+# concantentation
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
