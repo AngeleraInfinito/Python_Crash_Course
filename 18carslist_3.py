50ffce1c783f4256e78cf170cf7e84b4b947d614
AngeleraInfinito <Kritikahdy@gmail.com>
2026-04-14 12:44:28 +0530

    Create 18carslist_3.py

---
+++ 18carslist_3.py
@@ -0,0 +1,4 @@
+cars = ["bmw", "mercedes", "buggati"]
+print("I would like to own a " + cars[0] + ".")
+print("I would like to own a " + cars[1] + ".")
+print("I would like to own a " + cars[2] + ".")
