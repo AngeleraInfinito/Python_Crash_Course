6b258ce159c16682081628ea79b43c54967489db
AngeleraInfinito <Kritikahdy@gmail.com>
2026-04-14 13:17:41 +0530

    Create 23popmethod_3.py

---
+++ 23popmethod_3.py
@@ -0,0 +1,8 @@
+motorcycles = ["honda", "yamha", "suzuki"]
+print(motorcycles)
+
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+
+popped_motorcycle = motorcycles.pop(0)
+print(motorcycles)
