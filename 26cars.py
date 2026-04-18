92d8d128f822973249750d4e4af8013089d873a0
AngeleraInfinito <Kritikahdy@gmail.com>
2026-04-18 17:33:22 +0530

    Create 26cars.py

---
+++ 26cars.py
@@ -0,0 +1,11 @@
+# sorting a list permanently with sort method.
+
+# In alphabetical order
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+
+# in reverse alphabetical order
+cars_2 = ['bmw', 'audi', 'toyota', 'subaru']
+cars_2.sort(reverse=True)
+print(cars_2)
