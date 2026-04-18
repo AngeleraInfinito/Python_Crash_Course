c23a46ab1173e711ab01b324209f809e75a1881c
AngeleraInfinito <Kritikahdy@gmail.com>
2026-04-18 17:52:30 +0530

    Update 26cars.py

--- 26cars.py
+++ 26cars.py
@@ -4,8 +4,21 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 cars.sort()
 print(cars)
+print('\n')
 
 # in reverse alphabetical order
 cars_2 = ['bmw', 'audi', 'toyota', 'subaru']
 cars_2.sort(reverse=True)
 print(cars_2)
+print('\n')
+
+# sorting a list permanently with sorted method
+
+# in alphabetical order
+cars_3 = ['bmw', 'audi', 'toyota', 'subaru']
+print(sorted(cars_3))
+print('\n')
+
+# in reverse alphabetical order
+print(sorted(cars, reverse=True))
+print('\n')
