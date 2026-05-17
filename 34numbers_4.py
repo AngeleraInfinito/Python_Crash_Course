34f118623e25968484480c7b624e7f618bdf86ba
AngeleraInfinito <Kritikahdy@gmail.com>
2026-05-17 10:33:42 +0530

    Create 34numbers_4.py

---
+++ 34numbers_4.py
@@ -0,0 +1,10 @@
+for number in range(1,4):
+    print(number)
+
+# making list using range()
+numbers = list(range(1,5))
+print(numbers)
+
+# even-numbers
+even_numbers = list(range(2,11,2))
+print(even_numbers)
