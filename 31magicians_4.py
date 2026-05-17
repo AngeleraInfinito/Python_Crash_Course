b1312789e031f0eb7cea23b962150ca558fc2b27
AngeleraInfinito <Kritikahdy@gmail.com>
2026-05-17 10:11:59 +0530

    Update 31magicians_4.py

--- 31magicians_4.py
+++ 31magicians_4.py
@@ -1,3 +1,10 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
-    print(magician)
+    print(magician.title())
+
+# doing more in a for loop
+    print(magician.title() + ', that was a great trick.')
+    print('I look upto seeing your next trick,' + magician + '.\n')
+
+# doing something after a for loop
+print("Thank you, everyone. That was a great magic show!")
